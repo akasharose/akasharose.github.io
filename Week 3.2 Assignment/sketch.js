042e95c31ade88541779661def16937987cb988c
MarcusLaptop\mmilyko <marcusmilyko@gmail.com>
2016-09-19 12:45:49 -0600

    Added the base files for the Week 3.2 Assignement

---
+++ Week 3.2 Assignment/sketch.js
@@ -0,0 +1,15 @@
+var backRed;
+var backGreen;
+var backBlue;
+
+function setup() {
+  createCanvas (1280,720);
+  backRed = random (0, 255);
+  backGreen = random (0, 255);
+  backBlue = random (0, 255);
+  background (backRed, backGreen, backBlue);
+}
+
+function draw() {
+  
+}
