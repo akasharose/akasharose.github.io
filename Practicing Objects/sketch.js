48d64ac653885898e8b0d5a7406b1d7a8ea0960b
MarcusLaptop\mmilyko <marcusmilyko@gmail.com>
2016-09-26 14:17:36 -0600

    Finalize 4.1 Assignment, edit Object notes

--- Practicing Objects/sketch.js
+++ Practicing Objects/sketch.js
@@ -2,6 +2,8 @@
 var sadFace = {
   // Define all variables in the object
   faceDiam: 300,
+  posX: 400,
+  posY: 200,
   eyeY: 0,
   eyeX: 0,
   eyeXDiam: 0,
@@ -10,6 +12,7 @@ var sadFace = {
   mouthY: 0,
   mouthWidth: 0,
   mouthHeight: 0,
+  mouthOffset: 0,
 
   // This is a function defined in an Object, to be called in setup {}
   // The this. shows that every equation is grabbing a variable from THIS object
@@ -27,16 +30,23 @@ var sadFace = {
     stroke(0);
     translate(400, 200);
     // Create Face
-    fill (255,255,0)
-    ellipse(0, 0, sadFace.faceDiam, sadFace.faceDiam);
+    fill(255, 255, 0)
+    ellipse(this.posX, this.posY, this.faceDiam, this.faceDiam);
     // Create Left Eye
-    fill (0)
-    ellipse(-sadFace.eyeX, sadFace.eyeY, sadFace.eyeXDiam, sadFace.eyeYDiam);
+    fill(0)
+    ellipse(-this.eyeX, this.eyeY, this.eyeXDiam, this.eyeYDiam);
     // Create Right Eye
-    ellipse(sadFace.eyeX, sadFace.eyeY, sadFace.eyeXDiam, sadFace.eyeYDiam);
+    ellipse(this.eyeX, this.eyeY, this.eyeXDiam, this.eyeYDiam);
     // Creates Mouth
-    arc(sadFace.mouthX, sadFace.mouthY, sadFace.mouthWidth, sadFace.mouthHeight, PI, 0);
-    arc(sadFace.mouthX, sadFace.mouthY, sadFace.mouthWidth, sadFace.mouthHeight * (1 / 2), PI, 0);
+    arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight, PI + this.mouthOffset, 0 + this.mouthOffset);
+    arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight * (1 / 2), PI + this.mouthOffset, 0 + this.mouthOffset);
+  },
+  move: function() {
+    this.posX += 1
+  },
+
+  flipMouth: function() {
+    this.mouthOffset = (this.mouthOffset + PI) % TWO_PI;
   }
 }
 
@@ -48,4 +58,9 @@ function setup() {
 function draw() {
   background(0);
   sadFace.drawFace();
+  sadFace.move();
+}
+
+function mousePressed() {
+  sadFace.flipMouth();
 }
--- Week 4.1 Assignment/sketch.js
+++ Week 4.1 Assignment/sketch.js
@@ -3,11 +3,11 @@ var z = 0;
 var createCircles = true;
 var posX = true;
 var posY = true;
-var trueFalse = [ true , false ];
+var trueFalse = [true, false];
 // The Canvas can be changed to any size, and the diagram will work the same way
 function setup() {
-  createCanvas (900, 900);
-  rectMode (CENTER);
+  createCanvas(900, 900);
+  rectMode(CENTER);
   randomizeColors();
 }
 // Calls the shape drawing function, and increases the shift values
@@ -17,63 +17,64 @@ function draw() {
   z++;
 }
 // Runs the functions that change all the colors and return the shapes to their standard location
-function mousePressed () {
+function mousePressed() {
   randomizeColors();
   resetDirection();
   createCircles ^= true;
 }
 // Randomizes and sets the background and fill colors
-function randomizeColors () {
-  background (random (0,255), random (0,255), random (0,255));
-  fill (random (0,255), random (0,255), random (0,255));
+function randomizeColors() {
+  background(random(0, 255), random(0, 255), random(0, 255));
+  fill(random(0, 255), random(0, 255), random(0, 255));
 }
 // Creates the circles/squares, using if/else statements to find the direction they move
-function shapes () {
-  for (x = -1; x <= (width/100) + 1; x++) {
-    for (y = -1; y <= (height/100) + 1; y++) {
-    // Checks if the object is moving right or left
-    if (posX) {
-      // Checks if the object is moving up or down
-      if (posY) {
-        // Object creation in the Right and Down direction
-        if (createCircles) {
-          ellipse ((x * 100) + w, (y * 100) + z, 50, 50);
+function shapes() {
+  for (x = -1; x <= (width / 100) + 1; x++) {
+    for (y = -1; y <= (height / 100) + 1; y++) {
+      // Checks if the object is moving right or left
+      if (posX) {
+        // Checks if the object is moving up or down
+        if (posY) {
+          // Object creation in the Right and Down direction
+          if (createCircles) {
+            ellipse((x * 100) + w, (y * 100) + z, 50, 50);
+          } else {
+            rect((x * 100) + w, (y * 100) + z, 45, 45);
+          }
+          // The else of the up/down check
         } else {
-          rect ((x * 100) + w, (y * 100) + z, 45, 45);
+          // Object creation in the right and Up direction
+          if (createCircles) {
+            ellipse((x * 100) + w, (y * 100) - z, 50, 50);
+          } else {
+            rect((x * 100) + w, (y * 100) - z, 45, 45);
+          }
         }
-      // The else of the up/down check
+        // The else of the right/left check
       } else {
-        // Object creation in the right and Up direction
-        if (createCircles) {
-          ellipse ((x * 100) + w, (y * 100) - z, 50, 50);
+        if (posY) {
+          // Object creation in the left and Down direction
+          if (createCircles) {
+            ellipse((x * 100) - w, (y * 100) + z, 50, 50);
+          } else {
+            rect((x * 100) - w, (y * 100) + z, 45, 45);
+          }
         } else {
-          rect ((x * 100) + w, (y * 100) - z, 45, 45);
+          // Object creation in the left and Up direction
+          if (createCircles) {
+            ellipse((x * 100) - w, (y * 100) - z, 50, 50);
+          } else {
+            rect((x * 100) - w, (y * 100) - z, 45, 45);
+          }
         }
-      }
-    // The else of the right/left check
-    } else {
-      if (posY) {
-        // Object creation in the left and Down direction
-        if (createCircles) {
-          ellipse ((x * 100) - w, (y * 100) + z, 50, 50);
-        } else {
-          rect ((x * 100) - w, (y * 100) + z, 45, 45); }
-      } else {
-        // Object creation in the left and Up direction
-        if (createCircles) {
-          ellipse ((x * 100) - w, (y * 100) - z, 50, 50);
-        } else {
-          rect ((x * 100) - w, (y * 100) - z, 45, 45);
-        }
-      }
       }
     }
   }
 }
 // Randomizes the direction and shape type, resetting the location of the items.
-function resetDirection () {
-  posX = random (trueFalse);
-  posY = random (trueFalse);
+function resetDirection() {
+  posX = random(trueFalse);
+  posY = random(trueFalse);
   w = 0;
   z = 0;
 }
