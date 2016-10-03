54bc04655fc0fe57c0d41de3aff4aee1b94fae2e
MarcusLaptop\mmilyko <marcusmilyko@gmail.com>
2016-10-03 12:52:16 -0600

    Fixed in-class Practicing Objects example.

---
+++ Practicing Objects/sadFace.js
@@ -0,0 +1,70 @@
+// SadFace Class
+
+// Constructor Method for our Class "SadFace"
+function SadFace(faceD, scW, scH) {
+  this.faceDiam = faceD;
+  this.posX = random(scW);
+  this.posY = random(scH);
+
+  this.mouthOffset = 0;
+
+  this.dirX = 1;
+  this.dirY = 1;
+  this.maxDir = 10;
+
+  this.eyeY = 0 - this.faceDiam * (1 / 6);
+  this.eyeX = this.faceDiam * (1 / 6);
+  this.eyeXDiam = this.faceDiam * (1 / 10);
+  this.eyeYDiam = this.faceDiam * (1 / 5);
+  this.mouthX = 0;
+  this.mouthY = this.faceDiam * (1 / 6);
+  this.mouthWidth = this.faceDiam * (2 / 3);
+  this.mouthHeight = this.faceDiam * (1 / 5);
+  this.maxWidth = width;
+  this.maxHeight = height;
+
+  this.faceColorRed = random(256);
+  this.faceColorGreen = random(256);
+  this.faceColorBlue = random(256);
+
+}
+
+SadFace.prototype.drawFace = function() {
+  push();
+  fill(this.faceColorRed, this.faceColorGreen, this.faceColorBlue);
+  stroke(0);
+  // Sad Face Drawing
+  translate(this.posX, this.posY);
+  // Face
+  ellipse(0, 0, this.faceDiam, this.faceDiam);
+  // Eyes
+  fill(256);
+  ellipse(-this.eyeX, this.eyeY, this.eyeXDiam, this.eyeYDiam);
+  ellipse(this.eyeX, this.eyeY, this.eyeXDiam, this.eyeYDiam);
+  arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight, PI + this.mouthOffset, 0 + this.mouthOffset);
+  arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight * (1 / 2), PI + this.mouthOffset, 0 + this.mouthOffset);
+  pop();
+};
+
+SadFace.prototype.move = function() {
+  this.posX += this.dirX;
+  this.posY += this.dirY;
+  this.edgeCheck();
+};
+
+SadFace.prototype.flipMouth = function() {
+  this.mouthOffset = (this.mouthOffset + PI) % TWO_PI;
+};
+
+SadFace.prototype.edgeCheck = function() {
+  if (this.posX >= this.maxWidth) {
+    this.dirX = 0 - random(this.maxDir);
+  } else if (this.posY >= this.maxHeight) {
+    this.dirY = 0 - random(this.maxDir);
+  } else if (this.posX <= 0) {
+    this.dirX = random(this.maxDir);
+  } else if (this.posY <= 0) {
+    this.dirY = random(this.maxDir);
+  }
+
+};
--- Practicing Objects/sketch.js
+++ Practicing Objects/sketch.js
@@ -1,66 +1,29 @@
-// This creates an object
-var sadFace = {
-  // Define all variables in the object
-  faceDiam: 300,
-  posX: 400,
-  posY: 200,
-  eyeY: 0,
-  eyeX: 0,
-  eyeXDiam: 0,
-  eyeYDiam: 0,
-  mouthX: 0,
-  mouthY: 0,
-  mouthWidth: 0,
-  mouthHeight: 0,
-  mouthOffset: 0,
 
-  // This is a function defined in an Object, to be called in setup {}
-  // The this. shows that every equation is grabbing a variable from THIS object
-  defineFace: function() {
-    this.eyeY = 0 - this.faceDiam * (1 / 6);
-    this.eyeX = this.faceDiam * (1 / 6);
-    this.eyeXDiam = this.faceDiam * (1 / 10);
-    this.eyeYDiam = this.faceDiam * (1 / 5);
-    this.mouthY = this.faceDiam * (1 / 6);
-    this.mouthWidth = this.faceDiam * (2 / 3);
-    this.mouthHeight = this.faceDiam * (1 / 5);
-  },
-
-  drawFace: function() {
-    stroke(0);
-    translate(400, 200);
-    // Create Face
-    fill(255, 255, 0)
-    ellipse(this.posX, this.posY, this.faceDiam, this.faceDiam);
-    // Create Left Eye
-    fill(0)
-    ellipse(-this.eyeX, this.eyeY, this.eyeXDiam, this.eyeYDiam);
-    // Create Right Eye
-    ellipse(this.eyeX, this.eyeY, this.eyeXDiam, this.eyeYDiam);
-    // Creates Mouth
-    arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight, PI + this.mouthOffset, 0 + this.mouthOffset);
-    arc(this.mouthX, this.mouthY, this.mouthWidth, this.mouthHeight * (1 / 2), PI + this.mouthOffset, 0 + this.mouthOffset);
-  },
-  move: function() {
-    this.posX += 1
-  },
-
-  flipMouth: function() {
-    this.mouthOffset = (this.mouthOffset + PI) % TWO_PI;
-  }
-}
+// Create an empty array
+var sadFaces = [];
 
 function setup() {
+  // Define a Canvas
   createCanvas(800, 400);
-  sadFace.defineFace();
+  
+  // Create 'i' numbers of sadFace objects and push() them into the array
+  for (var i = 0; i < 100; i++) {
+    sadFaces[i] = (new SadFace(random(40, 100), 800, 400));
+  }
 }
 
 function draw() {
   background(0);
-  sadFace.drawFace();
-  sadFace.move();
+
+  for (var i = 0; i < sadFaces.length; i++) {
+    sadFaces[i].drawFace();
+    sadFaces[i].move();
+  }
+
 }
 
 function mousePressed() {
-  sadFace.flipMouth();
+  for (var i = 0; i < sadFaces.length; i++) {
+    sadFaces[i].flipMouth();
+  }
 }
