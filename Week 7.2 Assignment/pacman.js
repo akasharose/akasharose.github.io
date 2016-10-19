25a8f4fb81ef548d48693fdf08330afac1fac928
MarcusLaptop\mmilyko <marcusmilyko@gmail.com>
2016-10-19 11:50:16 -0600

    Uploaded first draft of the Week 7.2 Assignment

---
+++ Week 7.2 Assignment/pacman.js
@@ -0,0 +1,132 @@
+// Define the PacMan class
+
+// Constructor Method
+function PacMan(trans) {
+  // The Arc dimensions
+  this.pacDiam = 100;
+  this.posX = random(this.pacDiam / 2, width - this.pacDiam / 2);
+  this.posY = random(this.pacDiam / 2, height - this.pacDiam / 2);
+  this.startAngle = 1;
+  this.endAngle = 1;
+  // The Eye dimensions
+  this.eyeX = 1;
+  this.eyeY = 1;
+  this.eyeDiam = this.pacDiam / 8;
+  // The movement variables
+  this.pacMoveX = 1;
+  this.pacMoveY = 0;
+  this.pacSpeeds = [1, 2, 3, 4];
+  this.direct = 4;
+  this.newDirect = 1;
+  // The edges for the Pac-Pack
+  this.rightEdge = width - this.pacDiam / 2;
+  this.leftEdge = this.pacDiam / 2;
+  this.upEdge = this.pacDiam / 2;
+  this.downEdge = height - this.pacDiam / 2;
+  // The timer variables
+  this.framePassed = 0;
+  this.maxFrameGoal = 240;
+  this.frameGoal = random(this.maxFrameGoal/2, this.maxFrameGoal);
+}
+
+PacMan.prototype.drawPacman = function() {
+  fill(255, 238, 0);
+  arc(this.posX, this.posY, this.pacDiam, this.pacDiam, this.startAngle, this.endAngle);
+  fill(0);
+  ellipse(this.eyeX, this.eyeY, this.eyeDiam, this.eyeDiam)
+};
+
+PacMan.prototype.movePacman = function() {
+  this.posX += this.pacMoveX;
+  this.posY += this.pacMoveY;
+  this.eyeX += this.pacMoveX;
+  this.eyeY += this.pacMoveY;
+  this.wallCheck();
+};
+
+PacMan.prototype.wallCheck = function() {
+  if (this.posX >= this.rightEdge) {
+    this.stopPlz();
+    this.posX = width - this.pacDiam/2; // Makes sure things don't get caught outside the canvas
+  } else if (this.posX <= this.leftEdge) {
+    this.stopPlz();
+    this.posX = this.pacDiam/2;
+  } else if (this.posY >= this.downEdge) {
+    this.stopPlz();
+    this.posY = height - this.pacDiam/2;
+  } else if (this.posY <= this.upEdge) {
+    this.stopPlz();
+    this.posY = this.pacDiam/2;
+  }
+};
+
+PacMan.prototype.stopPlz = function() {
+  this.pacMoveX = 0;
+  this.pacMoveY = 0;
+};
+
+PacMan.prototype.pacDirect = function() {
+  var newDirect;
+  // Does this until the Direction is different
+  do {
+    (newDirect = floor(random(4)));
+  } while (newDirect == this.direct);
+  // Changes the direction variable
+  this.direct = newDirect;
+  // Actually changes the direction
+  if (this.direct === 0) {
+    this.pacDown();
+  } else if (this.direct == 1) {
+    this.pacRight();
+  } else if (this.direct == 2) {
+    this.pacUp();
+  } else if (this.direct == 3) {
+    this.pacLeft();
+  }
+};
+
+PacMan.prototype.pacDown = function() {
+  this.pacMoveY = random(this.pacSpeeds);
+  this.pacMoveX = 0;
+  this.startAngle = HALF_PI + QUARTER_PI / 2;
+  this.endAngle = HALF_PI - QUARTER_PI / 2;
+  this.eyeX = this.posX + this.pacDiam / 4;
+  this.eyeY = this.posY + this.pacDiam / 8;
+};
+
+PacMan.prototype.pacRight = function() {
+  this.pacMoveX = random(this.pacSpeeds);
+  this.pacMoveY = 0;
+  this.startAngle = QUARTER_PI / 2;
+  this.endAngle = -QUARTER_PI / 2;
+  this.eyeX = this.posX + this.pacDiam / 8;
+  this.eyeY = this.posY - this.pacDiam / 4;
+};
+
+PacMan.prototype.pacUp = function() {
+  this.pacMoveY = -random(this.pacSpeeds);
+  this.pacMoveX = 0;
+  this.startAngle = PI + HALF_PI + QUARTER_PI / 2;
+  this.endAngle = PI  + HALF_PI - QUARTER_PI / 2;
+  this.eyeX = this.posX - this.pacDiam / 4;
+  this.eyeY = this.posY - this. pacDiam / 8;
+};
+
+PacMan.prototype.pacLeft = function() {
+  this.pacMoveX = -random(this.pacSpeeds);
+  this.pacMoveY = 0;
+  this.startAngle = PI + QUARTER_PI / 2;
+  this.endAngle = PI - QUARTER_PI /2;
+  this.eyeX = this.posX - this.pacDiam / 8;
+  this.eyeY = this.posY - this.pacDiam / 4;
+};
+
+PacMan.prototype.timer = function() {
+  if (this.framePassed >= this.frameGoal) {
+    this.pacDirect();
+    this.framePassed = 0;
+    this.frameGoal = random(this.maxFrameGoal/2, this.maxFrameGoal);
+  } else {
+    this.framePassed++;
+  }
+}
---
+++ Week 7.2 Assignment/sketch.js
@@ -0,0 +1,63 @@
+pacPack = [];
+
+function setup() {
+  createCanvas(1000, 800);
+  frameRate(60);
+  for (var i = 0; i < 7; i++) {
+    pacPack[i] = (new PacMan());
+    pacPack[i].pacDirect();
+  }
+}
+
+function draw() {
+  background(200);
+  for (var i = 0; i < 7; i++) {
+    pacPack[i].drawPacman();
+    pacPack[i].movePacman();
+    pacPack[i].timer();
+    pacCheck();
+  }
+}
+
+function pacCheck() {
+  var curXPos = 0;
+  var curYPos = 0;
+  var curDist = 0;
+  var curXDir = true;
+  var curYDir = true;
+  var touchDist = 0;
+
+  for (var i = 0; i < pacPack.length; i++) {
+    curXPos = pacPack[i].posX;
+    curYPos = pacPack[i].posY;
+    touchDist = pacPack[i].pacDiam;
+    if (this.direct === 0) {
+      curYDir = true;
+    } else if (this.direct == 1) {
+      curXDir = true;
+    } else if (this.direct == 2) {
+      curYDir = false;
+    } else if (this.direct == 3) {
+      curXDir = false;
+    }
+    for (var j = i + 1; j < pacPack.length; j++) {
+      curDist = dist(curXPos, curYPos, pacPack[j].posX, pacPack[j].posY);
+      if (curDist <= touchDist && pacPack[j].posX > curXPos && curXDir == true) {
+        pacPack[i].stopPlz();
+        pacPack[j].stopPlz();
+      }
+      if (curDist <= touchDist && pacPack[j].posX < curXPos && curXDir == false) {
+        pacPack[i].stopPlz();
+        pacPack[j].stopPlz();
+      }
+      if (curDist <= touchDist && pacPack[j].posY > curYPos && curYDir == true) {
+        pacPack[i].stopPlz();
+        pacPack[j].stopPlz();
+      }
+      if (curDist <= touchDist && pacPack[j].posY < curYPos && curYDir == false) {
+        pacPack[i].stopPlz();
+        pacPack[j].stopPlz();
+      }
+    }
+  }
+}
