dacbe7a4f3b402aeed081c2a6d8d1da6643d8a29
MarcusLaptop\mmilyko <marcusmilyko@gmail.com>
2016-12-13 15:44:47 -0700

    Spawn Timer Fixed

--- finalProject/spawnTimers.js
+++ finalProject/spawnTimers.js
@@ -6,7 +6,7 @@ function enemyTimer() {
   console.log(pathed.length);
   if (timer === 0) {
     pathed.push(new PathedEnemy());
-    timer = 480 - timerShift;
+    timer = 180 - timerShift;
     if (timer <= 80) {
       timerShift === 0;
     } else if (timer <= 120) {
